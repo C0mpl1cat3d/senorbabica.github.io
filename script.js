a187866007889dc1c799151840fef235692d5c50
senorbabica <srameksimon5@gmail.com>
2022-05-07 00:40:51 +0200

    burgir

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const navbar = document.querySelector('div.navbar')
 const article = document.querySelectorAll('div.article')
 const navigation = document.querySelectorAll('.navigation>a')
+const hamburger = document.querySelector('#burger')
+const line = document.querySelectorAll('span.line')
 const sticky = window.pageYOffset;
 let width = window.innerWidth;
 AOS.init();
@@ -11,7 +13,6 @@ window.addEventListener('load', (event) => {
         for (let index = 0; index < navigation.length; index++) {
             navigation[index].remove();
         }
-        console.log('ASDASD')
     }
     
   });
@@ -19,12 +20,83 @@ window.addEventListener('load', (event) => {
 document.addEventListener('scroll', function () {
     if (window.pageYOffset > sticky) {
         navbar.classList.add("sticky")
-        article[0].classList.add("sticked")
     } else {
         navbar.classList.remove("sticky");
-        article[0].classList.remove("sticked")
     }
 
+})
+
+document.addEventListener('click', function(event){
+    if ((event.target === hamburger)&&!(hamburger.classList.contains('active'))){
+        hamburger.classList.add('active')
+        const line2Anim = line[1].animate([
+            {
+                transform: "rotate(90deg)",
+                opacity: "0"
+            }
+        ],
+        {
+            duration: 200, // 1.5s,
+            easing: "ease",
+            fill: "forwards"
+        }
+        )
+        const line1Anim = line[0].animate([
+            {
+                transform: "rotate(45deg) scale(1.25)"
+            }
+        ],
+        {
+            duration: 500,
+            easing: "ease",
+            fill: "forwards"
+        })
+        const line3Anim = line[2].animate([
+            {
+                transform: "rotate(-45deg) scale(1.25)"
+            }
+        ],
+        {
+            duration: 500,
+            easing: "ease",
+            fill: "forwards"
+        })
+    }
+    else if((event.target === hamburger)&&(hamburger.classList.contains('active'))){
+        hamburger.classList.remove('active')
+        const line2Anim = line[1].animate([
+            {
+                transform: "rotate(0)",
+                opacity: "1"
+            }
+        ],
+        {
+            duration: 200, // 1.5s,
+            easing: "ease",
+            fill: "forwards"
+        }
+        )
+        const line1Anim = line[0].animate([
+            {
+                transform: "rotate(0)"
+            }
+        ],
+        {
+            duration: 500,
+            easing: "ease",
+            fill: "forwards"
+        })
+        const line3Anim = line[2].animate([
+            {
+                transform: "rotate(0)"
+            }
+        ],
+        {
+            duration: 500,
+            easing: "ease",
+            fill: "forwards"
+        })
+    }
 })
 
 function checkVisible(elm) {
