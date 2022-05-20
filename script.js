222ef575e355b3251068ecf5f7c31707cec3fecb
senorbabica <srameksimon5@gmail.com>
2022-05-20 15:22:31 +0200

    s

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ document.addEventListener('dblclick', function (e) {
 })
 
 document.addEventListener("mouseover", function (event) {
-    if (event.target.classList.contains('img-container') || event.target.classList.contains('project-img')) {
+    if (event.target.classList.contains('img-container') || event.target.classList.contains('project-img') || event.target.classList.contains('blog-img')) {
         currentIcon = event.target.querySelector('i.fa-magnifying-glass-plus')
         showIcon(currentIcon);
     }
@@ -77,18 +77,12 @@ document.addEventListener("mouseover", function (event) {
         currentIcon = event.target;
         showIcon(currentIcon)
     }
-    else if (!event.target.classList.contains('img-container') || !event.target.classList.contains('project-img')) {
+    else if (!event.target.classList.contains('img-container') || !event.target.classList.contains('project-img') || event.target.classList.contains('blog-img')) {
         for (const i of icons) {
             hideIcon(i)
         }
     }
 })
-
-function checkVisible(elm) {
-    let rect = elm.getBoundingClientRect();
-    let viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
-}
 
 function openOverlay() {
     hamburger.classList.add('active')
